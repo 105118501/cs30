b6934f27ea639e21cd0bef5fafb6809397a97340
105118501 <105118501@L815WD220006.edu.cbe.ab.ca>
2022-09-12 12:45:28 -0600

    12:44 pm

---
+++ CH10/src/DivisibleBy3.java
@@ -0,0 +1,25 @@
+/*
+
+Program: DivisibleBy3.java          Last Date of this Revision: September 30, 2019
+
+Purpose: An application that uses nested for loops to output a table that diplays number in 
+ multiples of ten.
+
+Author: Your Name, 
+School: CHHS
+Course: Computer Programming ??
+ 
+
+*/
+public class DivisibleBy3 
+{
+
+}
+
+
+
+/* Screen Dump
+ 
+Paste the output of your code here
+ 
+ */
