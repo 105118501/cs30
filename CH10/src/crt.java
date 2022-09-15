44fbd39aec84e5517ad959b4b2a02c66c7a6df70
105118501 <105118501@L815WD220006.edu.cbe.ab.ca>
2022-09-15 14:28:40 -0600

    2;28

--- CH10/src/crt.java
+++ CH10/src/crt.java
@@ -2,12 +2,12 @@
 public class crt 
 {
 	
-/*1.
- *2.
- *3.
- *4.
- *5.
- *6.
+/*1.a window 
+ *2.when event on do ...
+ *3.you need a panel to add things on
+ *4.yes, can change texts/icon when action is on
+ *5.so its not running all the time or getting wrong inputs  
+ *6.label the user can't edit it 
  */
 	
 }
