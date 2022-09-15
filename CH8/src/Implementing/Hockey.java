74c46a1207c8d174e98c8101b2af626598e51d96
105118501 <105118501@L815WD220006.edu.cbe.ab.ca>
2022-09-15 15:29:40 -0600

    3;29

---
+++ CH8/src/Implementing/Hockey.java
@@ -0,0 +1,22 @@
+package Implementing;
+
+public class Hockey 
+{
+	 PuckPart1 pp1= new  PuckPart1();
+	
+	
+	
+	 public static void main(String args[])
+	    {
+	       
+		 
+		 
+		 
+		 
+		 
+		 
+	       
+	    }
+
+	
+}
---
+++ CH8/src/Implementing/PuckPart1.java
@@ -0,0 +1,28 @@
+package Implementing;
+
+public class PuckPart1 
+{
+
+	boolean sta, you;
+	
+	double wei,thi,dia;
+	
+	
+	
+	
+	public double getWeigth() 
+	{
+		return(wei);
+	}
+	
+	public double getDivision() 
+	{
+		return(dia);
+	}
+	
+	public String toString() 
+	{
+		String out="";
+		return(out);
+	}
+}
