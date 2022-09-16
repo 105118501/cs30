d08cc18b48da6540902e43e3e92d077e5172e5e7
105118501 <105118501@L815WD220006.edu.cbe.ab.ca>
2022-09-16 11:42:05 -0600

    11;41

--- CH8/src/Implementing/Hockey.java
+++ CH8/src/Implementing/Hockey.java
@@ -1,19 +1,53 @@
 package Implementing;
 
+import java.util.Scanner;
+
 public class Hockey 
 {
-	 PuckPart1 pp1= new  PuckPart1();
+	 
 	
 	
 	
 	 public static void main(String args[])
 	    {
-	       
+		 PuckPart1 pp1= new  PuckPart1();
+		 Scanner input = new Scanner(System.in);
 		 
 		 
 		 
+		 System.out.println("The of your puck weigth is: ");
+		 double w = input.nextDouble();
+		 pp1.StaYou(w);
 		 
-		 
+		boolean y = pp1.getYouth();
+		boolean s = pp1.getStandard();
+		if (y==true) 
+		{
+			System.out.println("Youth");	
+		}
+		else if (s==true) 
+		{
+			System.out.println("Standard");
+			
+		}
+		else if (y == false && s == false) 
+		{
+			System.out.println("is your puck 1 inch thick (y/n): ");	
+			String  yn = input.next();
+			if (yn == "y" ) 
+			{
+				System.out.println("you have a offical hockey puck");
+			}
+			else 
+			{
+				System.out.println("youdo not have a offical hockey puck");
+			}
+		
+		
+		}
+		
+		
+		
 		 
 	       
 	    }
--- CH8/src/Implementing/PuckPart1.java
+++ CH8/src/Implementing/PuckPart1.java
@@ -3,12 +3,38 @@ package Implementing;
 public class PuckPart1 
 {
 
-	boolean sta, you;
+	boolean sta=false, you=false;
 	
 	double wei,thi,dia;
 	
+	public void StaYou (double w) 
+	{
+		wei = w;
+		sta=false;
+		you=false;
+		
+		if (wei >= 4 && wei <= 4.5) 
+		{
+			you = true ;
+			
+		}
+		else if (wei >= 5 && wei <= 5.5)
+		{
+			sta= true ;
+			
+		}
+		
+		
+	}
 	
-	
+	public boolean getYouth() 
+	{
+		return(you);
+	}
+	public boolean getStandard() 
+	{
+		return(sta);
+	}
 	
 	public double getWeigth() 
 	{
